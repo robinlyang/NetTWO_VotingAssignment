f4433f0bbcb6514000861738379a8156067803c0
robinlyang <robiny@gmail.com>
2016-11-23 18:00:47 -0700

    Initial Project

--- NetTWO_VotingAssignment/Program.cs
+++ NetTWO_VotingAssignment/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace NetTWO_VotingAssignment
 {
@@ -11,6 +12,7 @@ namespace NetTWO_VotingAssignment
         static void Main(string[] args)
         {
             string[] voting = { "X012033001140", "C075100026080", "A050070060100", "Q040088050090" };
+            SortedList sortedVotes = new SortedList();
             //Print out Table
 
 
